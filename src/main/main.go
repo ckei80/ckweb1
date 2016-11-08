af9a468a236a717866248f85fe70cc30759b7046
Daniel Heden <daniel.heden@evokegaming.com>
2016-11-08 17:04:26 +0100

    Improve main.go with new structure

--- src/main/main.go
+++ src/main/main.go
@@ -1,49 +1,59 @@
 package main
 
 import (
-	"io/ioutil"
-	"log"
+	"html/template"
 	"net/http"
-	"strings"
+	"os"
 )
 
-type MainHandler struct {
+//Compile templates on start
+var templates = template.Must(template.ParseFiles("templates/header.html", "templates/footer.html", "templates/main.html", "templates/about.html"))
+
+//A Page structure
+type Page struct {
+	Title string
 }
 
-func (this *MainHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	path := r.URL.Path[1:]
-	log.Println(path)
+type justFilesFilesystem struct {
+	fs http.FileSystem
+}
 
-	data, err := ioutil.ReadFile(string(path))
-
-	if err != nil{
-		w.WriteHeader(404)
-		w.Write([]byte("404 error - " + http.StatusText(404)))
-	}else{
-		var contentType string
-
-		if strings.HasSuffix(path, ".css"){
-			contentType = "text/css"
-		}else if strings.HasSuffix(path, ".html"){
-			contentType = "text/html"
-		}else if strings.HasSuffix(path, ".js"){
-			contentType = "application/javascript"
-		}else if strings.HasSuffix(path, ".png"){
-			contentType = "image/png"
-		}else if strings.HasSuffix(path, ".svg"){
-			contentType = "image/svg+xml"
-		}else {
-			contentType = "text/plain"
-		}
-
-		w.Header().Add("Content-Type", contentType)
-		w.Write(data)
-
+func (fs justFilesFilesystem) Open(name string) (http.File, error) {
+	f, err := fs.fs.Open(name)
+	if err != nil {
+		return nil, err
 	}
+	return neuteredReaddirFile{f}, nil
+}
 
+type neuteredReaddirFile struct {
+	http.File
+}
+
+func (f neuteredReaddirFile) Readdir(count int) ([]os.FileInfo, error) {
+	return nil, nil
+}
+
+//Display the named template
+func display(w http.ResponseWriter, tmpl string, data interface{}) {
+	fs := justFilesFilesystem{http.Dir("content/css/")}
+	http.Handle("/content/css/", http.StripPrefix("/content/css/", http.FileServer(fs)))
+	templates.ExecuteTemplate(w, tmpl, data)
+}
+
+//The handlers.
+func mainHandler(w http.ResponseWriter, r *http.Request) {
+	display(w, "main", &Page{Title: "Home"})
+}
+
+func aboutHandler(w http.ResponseWriter, r *http.Request) {
+	display(w, "about", &Page{Title: "About"})
 }
 
 func main() {
-	http.Handle("/", new(MainHandler))
+	http.HandleFunc("/", mainHandler)
+	http.HandleFunc("/about", aboutHandler)
+
+	//Listen on port 8080
 	http.ListenAndServe(":8080", nil)
 }
